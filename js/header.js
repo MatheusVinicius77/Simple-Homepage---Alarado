58d8c693eeb19f3a73a93349bd060769d710a2e8
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-01-25 11:12:03 -0300

    feat: add responsive navbar

---
+++ js/header.js
@@ -0,0 +1,19 @@
+const hamburguer_button = document.querySelector('i');
+const hamburguer_sidebar = document.querySelector('.hamburguer-sidebar');
+const nav_link = document.querySelectorAll('.nav-link').item(1);
+const buttom_mode = document.querySelectorAll('.button-mode').item(1);
+const close_button = document.querySelector('.close-button');
+
+hamburguer_button.addEventListener('click', () => {
+    console.log('cliquei');
+    hamburguer_sidebar.style.display = 'flex';
+    nav_link.style.display = 'block'
+    buttom_mode.style.display = 'block'
+});
+
+close_button.addEventListener('click', () => {
+    hamburguer_sidebar.style.display = 'none'
+});
+
+
+console.log(close_button);
