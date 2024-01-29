379bc3a0058a893234efa0c1c2cbeac72224f5ab
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-01-29 10:00:49 -0300

    fix: transition sidebar and font color

--- js/header.js
+++ js/header.js
@@ -1,25 +1,23 @@
 const hamburguer_button = document.querySelector('i');
-const nav_wrapper = document.querySelector('.nav-wrapper')
-const close_button = document.querySelector('.close-button')
-console.log(close_button)
-
+const nav_wrapper = document.querySelector('.nav-wrapper');
+const close_button = document.querySelector('.close-button');
+const nav_list = document.querySelectorAll('.nav__list>li>a');
 hamburguer_button.addEventListener('click', () => {
-    const visible = nav_wrapper.getAttribute('data-visible')
+    const visible = nav_wrapper.getAttribute('data-visible');
 
     if (visible === 'false') {
-        nav_wrapper.setAttribute('data-visible', 'true')
-        close_button.setAttribute('data-visible', 'true')
+        nav_wrapper.setAttribute('data-visible', 'true');
+        close_button.setAttribute('data-visible', 'true');
 
     } else {
-        nav_wrapper.setAttribute('data-visible', 'false')
+        nav_wrapper.setAttribute('data-visible', 'false');
     }
 })
 
 
 close_button.addEventListener('click', () => {
-    nav_wrapper.setAttribute('data-visible', 'false')
-    close_button.setAttribute('data-visible', 'false')
-
+    nav_wrapper.setAttribute('data-visible', 'false');
+    close_button.setAttribute('data-visible', 'false');
 })
 
 
